792ace4e1e40d7e266326cbf7b81efa8b7a33458
apolline18 <62940019+apolline18@users.noreply.github.com>
2024-02-07 11:08:58 +0100

    feat: create  top 30 folder and insert images

--- general_script2.py
+++ general_script2.py
@@ -1,69 +1,61 @@
-#from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from collections import defaultdict
+import heapq
 import os
-import pandas as pd
-import pathlib
-from pathlib import Path
+import shutil
+""" Defition des liens"""
+file_path = './media/identity_CelebA.txt'
+image_folder = './media/img_align_celeba/'
+top30_folder = './media/top30_folder/'
 
-CELEB_IDENTITY_FILE = "./celeb_mappings/identity_CelebA.txt"
-identity_file = Path(CELEB_IDENTITY_FILE)
+"""Trier le top 30 des classes les plus représentées"""
+def get_most_represented_labels(file_path, top_count=30):
+    # Lire le fichier texte d'image
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
 
-#recuperer la liste du top 30 
-def extract_most_represented_faces( identity_file):
-        # process the identity file to have a matrix which we will use to classify the faces
-        identity_matrix = pd.read_csv(identity_file, sep=" ", header=None)
-        identity_matrix.columns = ["FileName", "Label"]
+    # Sauvegarder les classes et leur fréquence d'apparition
+    labels_count = defaultdict(int)
+    for line in lines:
+        parts = line.split()
+        if len(parts) == 2:
+            labels_count[parts[1]] += 1
 
-        # find and extract as a list which labels are more represented
-        most_represented_labels = identity_matrix['Label'].value_counts()[:30].index.values
-        most_represented_extract = identity_matrix[identity_matrix['Label'].isin(most_represented_labels)]
-        # most_represented_pictures : array of most represented faces filenames we want to extract
-        most_represented_filenames_list = most_represented_extract["FileName"].tolist()
-        #most_represented_identity_list = most_represented_extract["Label"].tolist()
+    # Get the 50 most frequent classes
+    top_labels = heapq.nlargest(top_count, labels_count, key=labels_count.get)
 
-        return most_represented_filenames_list
-# faire un dossier top 30
-def create_imagesFolder():
-    #verifier si le dossier top 30 n'existe pas déja
-    if not os.path.exists("top30"):
-        os.mkdir("top30")
-    
-        #creer les sous dossiers
-        for i in range ( 1, 30 +1):
-            nom_sous_dossier =  f"top30/classe_{i}"
-            try:
-                os.mkdir(nom_sous_dossier)
-            except FileExistsError:
-                print(f"Le sous-dossier '{nom_sous_dossier}' existe déjà.")
-    else:
-        print(f"Le dossier existe déjà.")
-            
-create_imagesFolder()
-most_represented_filenames = extract_most_represented_faces(identity_file)
-print(f"liste '{most_represented_filenames}'")
+    return top_labels
 
-"""
-#data pre-processing and data augmentation (transform image to create new one)
-datagen = ImageDataGenerator(
-        rotation_range=40,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        fill_mode='nearest')
+# Appel de la fonction "top classes"
+top_labels = get_most_represented_labels(file_path)
+
+# Afficher le top 30 
+for i, labels in enumerate(top_labels, start=1):
+    print(i, labels)
+
+"""Deplacer les images correspondant au top 30 dans les sous dossiers"""
+def create_folders(top30_folder, top_labels) :
+    # Creer un fichier pour chaque classe
+    for label in top_labels:
+        label_folder = os.path.join(top30_folder, label)
+        os.makedirs(label_folder, exist_ok=True)
+        
+def organize_images_by_labels(file_path, image_folder, top30_folder, top_count=30):
+    # Recuper le top 30 des classes
+    top_label = get_most_represented_labels(file_path, top_count=30)
+
+    create_folders(top30_folder, top_label)
+    # Lire le fichier texte et deplacer les images au bon endroit
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+        for line in lines:
+            parts = line.split()
+            if len(parts) == 2:
+                image_name, label = parts
+                source_path = os.path.join(image_folder, image_name)
+                destination_folder = os.path.join(top30_folder, label)
+                if os.path.exists(destination_folder):
+                    shutil.copy(source_path, destination_folder)
 
 
-#Generate news pictures
-img = load_img('data/train/cats/cat.0.jpg')  # this is a PIL image
-x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
-x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
-
-# the .flow() command below generates batches of randomly transformed images
-# and saves the results to the `preview/` directory
-i = 0
-for batch in datagen.flow(x, batch_size=1,
-                          save_to_dir='preview', save_prefix='cat', save_format='jpeg'):
-    i += 1
-    if i > 20:
-        break  # otherwise the generator would loop indefinitely
-"""
+organize_images_by_labels(file_path, image_folder, top30_folder, top_count=30)  
